e3044874b4f5893b3caac9858956ed8cc8ec76b6
gustavmario96-ctrl <gustavmario96@gmail.com>
2026-09-03 11:45:45 +0200

    Item, Student und Attack

---
+++ Attack.java
@@ -0,0 +1,14 @@
+public class Attack{
+    public String name;
+    public String subject;
+    public String teacher;
+    public int damage;
+    
+    public Student(String nameNeu, String subjectNeu, String teacherNeu, int damageNeu){
+        name = nameNeu;
+        subject = subjectNeu;
+        teacher = teacherNeu;
+        damage = damageNeu;
+    }
+   
+}
